a716ca1347db53e19053b25c0e341a8bd1a8a37f
vojtdaa <vojtechrychta@seznam.cz>
2025-06-19 12:53:14 +0200

    Update daviduv problem.py

--- funkce/daviduv problem.py
+++ funkce/daviduv problem.py
@@ -1,16 +1,24 @@
 cisla = []
 n = 5
 k = 3
+
 for i in range(n):
     cisla.append(i+1)
 
+skok = k-1
+i = 0
+def FindNumber():
+    x = 1
+    while x > 0:
+        if cisla[x] != 0:
+            return cisla[x]
 
-for i in range(n-1):
-    index = 2*(i+1)
-    if index > n:
-        index -= n
+    
+for i in range(n):
+    if cisla[i] == 3:
+        cisla[i] = 0
     print(cisla)
-    cisla.pop[index]
+    
 
 
 
