71f8d216224ec226505f38021a70a075e4ec5580
vojtdaa <vojtechrychta@seznam.cz>
2025-10-13 22:05:19 +0200

    cvnvb

--- line_and_points.py
+++ line_and_points.py
@@ -1,43 +1,81 @@
 import matplotlib.pyplot as plt
 
-points = [(5, 10), (2, -2), (-7, -3)]
+points = [(1, 1), (2, 2), (2, 7), (-9, -7), (-7, 8), (6, -8)]
 
 x_points = [p[0] for p in points]
 y_points = [p[1] for p in points]
+
+min_point = min(x_points)
+max_point = max(x_points)
 
 for i in range(len(x_points)):
     plt.plot(x_points[i], y_points[i], marker=".", color='red', markersize=8)
 
 
-x_ratio = [n for n in range(-10, 11, 1)]
+x_ratio = [n for n in range(min_point, max_point+1, 1)]
 
 # f: y = ax + b
 
-a = 1
+a = 4
 b = -0.5
+
+start_a = a
+start_b = b
+
+step = 0.1
+l_rate = 0.01
+pocet_iteraci = 10000
 
 y_ratio = [a*n + b for n in x_ratio]
 
 def Primka():
     plt.plot(x_ratio, y_ratio, label = "", color = "#000000")
 
-def Difference_pa():
-    dif = (y_points[0]-(a*x_points[0] + b))**2
-    dif2 = (y_points[1]-(a*x_points[1] + b))**2
-    return dif + dif2
+def LossFunction(a, b):
+    return sum((y - (x*a + b))**2 for x, y in points)/len(x_points)
 
-values = []
-val_min = Difference_pa()
 
-for _ in range(100):
-    dif = Difference_pa()
-    if dif < val_min:
-        val_min = dif
-    values.append(dif)
-    b+=1
+def BestA(a, step=0.1):
+    for _ in range(pocet_iteraci):
+        loss_now = LossFunction(a, b)
+        loss_after = LossFunction(a + step, b)
+    
+        if loss_now < loss_after:
+            step = -step
+        a += step
+        step *= 0.99
+    return a
 
-print(val_min)
+def BestB(b, step=0.1):
+    for _ in range(pocet_iteraci):
+        loss_now = LossFunction(a, b)
+        loss_after = LossFunction(a, b + step)
+    
+        if loss_now < loss_after:
+            step = -step
+        b += step
+        step *= 0.99
 
+    return b
+
+def GetIdealValues(a, b, l_rate = 0.01):
+    for _ in range(pocet_iteraci):
+       
+        da = sum(-2*(x * (y - (a*x + b))) for x, y in points)/len(x_points)
+        db = sum(-2* (y - (a*x + b)) for x, y in points)/len(x_points)
+
+        a = a - l_rate * da
+        b = b - l_rate * db
+    
+    return a, b
+
+a, b = GetIdealValues(a, b, l_rate)
+
+print(f"a: {start_a}", f"-> {a}")
+print(f"b: {start_b}", f"-> {b}")
+print(f"loss function = {LossFunction(a, b)}")
+
+y_ratio = [a*n + b for n in x_ratio]
 Primka()
 
 plt.axhline(0, color='black', linestyle='--', linewidth=1)
