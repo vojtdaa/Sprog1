0621b89d2b6eee23517ea231567cd91ead620d79
vojtdaa <vojtechrychta@seznam.cz>
2025-10-20 22:32:44 +0200

    dfsd

---
+++ fun/tictactoe.py
@@ -0,0 +1,72 @@
+
+
+all_points = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+x_points = []
+o_points = []
+
+desk = ["-"] * 9
+
+win_combs = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 5, 9), (3, 5, 7), (1, 4, 7), (2, 5, 8), (3, 6, 9)]
+
+def Check():
+    vysledek = 0
+    for comb in win_combs:
+
+        if all(point in x_points for point in comb):
+            vysledek = 1
+            return vysledek
+        
+        if all(point in o_points for point in comb):
+            vysledek = 2
+            return vysledek
+    return vysledek
+
+def Play():
+    game_running = True
+    vysledek = 0
+    play = 0
+    while game_running:
+        play = int(input("Hrac x: Zadej cislo 1-9: "))
+        if play in all_points:
+            x_points.append(play)
+            all_points.remove(play)
+        else: print("Invalid move.")
+
+        Display()
+        vysledek = Check()
+        if vysledek == 1:
+            return vysledek
+        if vysledek == 2:
+            return vysledek
+
+        play = int(input("Hrac o: Zadej cislo 1-9: "))
+        if play in all_points:
+            o_points.append(play)
+            all_points.remove(play)
+        else: print("Invalid move.")
+
+        Display()
+        vysledek = Check()
+        if vysledek == 1:
+            return vysledek
+        if vysledek == 2:
+            return vysledek
+
+        
+
+def Display():
+    for i in range(1, 10):
+        if i in x_points:
+            desk[i-1] = "x"
+        if i in o_points:
+            desk[i-1] = "o"
+    print(desk[0], desk[1], desk[2])
+    print(desk[3], desk[4], desk[5])
+    print(desk[6], desk[7], desk[8])
+
+
+print("hraci pole:")
+print("1 2 3\n4 5 6\n7 8 9")
+game_result = Play()
+print(f"The winner is Player {game_result}")
