884f56a48dcc9deecc0e8e645dc3c7f41291b2df
vojtdaa <vojtechrychta@seznam.cz>
2025-10-23 22:46:53 +0200

    fgfg

---
+++ fun/merge_interval.py
@@ -0,0 +1,20 @@
+seznam = [[1,3], [2,5], [10,12], [11,15]]
+
+new = [2]
+stop = False
+complete = False
+print(new)
+while not stop:
+    i = 0
+    j = 0
+    a = seznam[i][j]
+    while a != seznam[i][j+1]:
+        a = seznam[i][j]
+        new.append(a)
+        a += 1
+        if a>10: break
+    break
+
+     
+
+print(new)
