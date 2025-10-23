59a9abc400fb5eaae6b6744d6576b052410f3cca
vojtdaa <vojtechrychta@seznam.cz>
2025-10-24 00:44:59 +0200

    hj

--- fun/merge_interval.py
+++ fun/merge_interval.py
@@ -1,22 +1,36 @@
 seznam = [(1,3), (2,5), (10,12), (11,15)]
 
 new = []
-stop = False
-complete = False
 
-def MakeNew():
-    for z in range(len(seznam)-1):
-        Rozepis_interval(z, seznam, new)  
-    return
+
 
 def Rozepis_interval(start, seznam, new):
     a = seznam[start][0]
     while a <= seznam[start][1]:
         if a not in new:
             new.append(a)
-            a += 1
+        a += 1
     return new 
-   
+
+def MakeNew():
+    for z in range(len(seznam)):
+        Rozepis_interval(z, seznam, new)   
+  
 MakeNew()
 
+def SliceIt(new):
+    complete = []
+    a = 0
+    b = 0
+    while a < len(new):
+        while b <= len(new)-1 and new[b] == (new[a]+(b-a)):
+            b += 1
+
+        complete.append((new[a], new[b-1]))
+        a = b
+        
+    return complete
+
+
 print(new)
+print(SliceIt(new))
---
+++ fun/merge_intervals_v2.py
@@ -0,0 +1,31 @@
+seznam = [(5, 6), (1, 3), (2, 4), (8, 10), (9, 11), (15, 18), (17, 20)]
+seznam = sorted(seznam)
+
+def Merge(seznam):
+    start = 0
+    step = 0
+    while start <= len(seznam)-2:
+        min_val = seznam[start][0]
+        max_val = seznam[start][1]
+
+        new1 = seznam[start+1][0]
+        new2 = seznam[start+1][1]
+        if new1 >= min_val and new1 <= max_val:
+            if new2 <= max_val:
+                seznam[start] = (min_val, max_val)
+                seznam.pop(start+1)
+            elif new2 > max_val:
+                seznam[start] = (min_val, new2)
+                seznam.pop(start+1)
+        elif new2 > min_val and new2 <= max_val:
+            seznam[start] = (new1, max_val)
+            seznam.pop(start+1)
+        elif new1 < min_val and new2 > max_val:
+            seznam[start] = (new1, new2)
+            seznam.pop(start+1)   
+        else: 
+            start += 1 
+        step += 1
+    return seznam, step
+
+print(Merge(seznam))
