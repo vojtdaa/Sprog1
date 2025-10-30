e9530783139b426e24f2b718aa049469a2f45598
vojtdaa <vojtechrychta@seznam.cz>
2025-10-30 13:04:52 +0100

    ppú

---
+++ tridici algoritmy/merge_sort.py
@@ -0,0 +1,39 @@
+
+seznam = [5, 7, 8, 9, 6, 4, 1, 3, 10]
+merge_steps = 0
+def Merge(left, right):
+    global merge_steps
+    i = 0
+    j = 0
+    new = []
+    while i < len(left) and j < len(right):
+        merge_steps += 1
+        if left[i] <= right[j]:
+            new.append(left[i])
+            i += 1
+        else:
+            new.append(right[j])
+            j += 1
+    while i < len(left):
+        merge_steps += 1
+        new.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merge_steps+= 1
+        new.append(right[j])
+        j += 1
+    print(merge_steps)
+    return new
+
+def Merge_sort(seznam):
+    if len(seznam) == 1:
+        return seznam
+    middle = len(seznam)//2
+    left = Merge_sort(seznam[:middle])
+    right = Merge_sort(seznam[middle:])
+
+    
+    return Merge(left, right)
+
+print(Merge_sort(seznam))
