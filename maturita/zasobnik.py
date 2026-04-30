71e3a7fb0e9f4294cce8780f4c17c084b1021184
vojtdaa <vojtechrychta@seznam.cz>
2026-04-30 15:55:58 +0200

    hg

--- maturita/zasobnik.py
+++ maturita/zasobnik.py
@@ -2,18 +2,41 @@ class Prvek:
     def __init__(self, hodnota, predchozi):
         self.hodnota = hodnota
         self.predchozi = predchozi
+    
+    def __str__(self):
+        return str(self.hodnota)
 
 class Zasobnik:
-    def __init__(self, peak = None):
-        self.peak = peak
+    def __init__(self):
+        self.top = None
 
     def novy_prvek(self, hodnota):
-        novy = Prvek(hodnota, self.peak)
-        self.peak = novy
-        return self.peak
+        self.top = Prvek(hodnota, self.top)
+        return self.top
     
     def peak(self):
-        return self.peak
+        return self.top
+    
+    def pop(self):
+        if self.top != None:
+            self.top = self.top.predchozi
+        return self.top
+    
+    def isEmpty(self):
+        if self.top == None:
+            is_empty = True
+        else: is_empty = False
 
+        return is_empty
 
-print(Zasobnik.novy_prvek())
+z = Zasobnik()
+
+z.novy_prvek(26)
+z.novy_prvek(15)
+print(z.peak())
+z.pop()
+print(z.peak())
+print(z.isEmpty())
+z.pop()
+z.pop()
+print(z.isEmpty())
