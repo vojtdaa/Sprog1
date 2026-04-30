170681dd61a23f6301f2cb5b4279049ece1ac091
vojtdaa <vojtechrychta@seznam.cz>
2026-04-30 14:39:23 +0200

    hj

--- maturita/zasobnik.py
+++ maturita/zasobnik.py
@@ -1,11 +1,19 @@
 class Prvek:
-    def __init__(self, hodnota, predchozi = None):
+    def __init__(self, hodnota, predchozi):
         self.hodnota = hodnota
         self.predchozi = predchozi
 
 class Zasobnik:
-    def __init__(self, peak = None, last = None):
+    def __init__(self, peak = None):
         self.peak = peak
 
-    def __novy_prvek__(self, hodnota):
-        novy_prvek = Prvek(hodnota)
+    def novy_prvek(self, hodnota):
+        novy = Prvek(hodnota, self.peak)
+        self.peak = novy
+        return self.peak
+    
+    def peak(self):
+        return self.peak
+
+
+print(Zasobnik.novy_prvek())
